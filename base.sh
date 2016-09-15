bbf454580f7f32f62438d4e59f10aebc2832e887
Koji Tokubo <koji.tokubo@optim.co.jp>
2016-09-15 11:06:25 +0900

    yep

--- base.sh
+++
@@ -1,1 +0,0 @@
-export DBPATH=$HOME/Dropbox/
--- minibuffer.sh
+++ minibuffer.sh
@@ -1,3 +1,4 @@
-MINIBUFFERPATH=$DBPATH/minibuffer/
+MINIBUFFERPATH=$DBPATH/warez/bin/minibuffer/
 export MINIBUFFERPATH
-
+MB=$HOME/Desktop/temp.md
+export MB

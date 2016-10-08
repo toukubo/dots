5bfc8a9863899825c40942496b4560d546373fe7
toukubo <toukubo@nnnnnnnnnn-no-MacBook-Air.local>
2016-10-08 21:01:05 +0900

    and other status dirs more

--- dirs.sh
+++ dirs.sh
@@ -1,2 +1,4 @@
 export MNG=/Users/toukubo/Dropbox/nodes/inbox/management.run
 export done=/Users/toukubo/Dropbox/nodes/archived/done
+export fix=/Users/toukubo/Dropbox/nodes/fix
+export better=/Users/toukubo/Dropbox/nodes/better

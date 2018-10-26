e6252ea4fc59234879405617bdea131b1d88048a
koji toukubo <toukubo@gmail.com>
2018-10-26 15:59:38 +0900

    nodejs

--- 0.base.sh
+++ 0.base.sh
@@ -1,4 +1,4 @@
 
 export gitdir=$HOME
 export DBPATH=$HOME/Dropbox/
-export GDPATH=$HOME/toukubo@gmail.com/
+export GDPATH=$HOME/toukubo\@gmail.com/
--- minibuffer.sh
+++ minibuffer.sh
@@ -1,4 +1,4 @@
 MINIBUFFERPATH=$GDPATH/warez/bin/minibuffer/
 export MINIBUFFERPATH
-MB=$GDPATH/mb/temp.md
+MB=$DBPATH/mb.md
 export MB
